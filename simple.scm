8909e641415dfe4cd4a8e84dd9e146df76a23b7e
ptx <ptx@openengine.dk>
2010-10-23 19:06:42 +0000

    Testing ProcessEvent from scheme

--- simple.scm
+++ simple.scm
@@ -1,22 +1,49 @@
+
 ;; use this demo with:
 ;;   ./make.py Simple
 ;;   ./build/GambitDemo/simple
 
+
 (c-declare #<<header
+
+#include <Logging/Logger.h>
+#include <Core/IEngine.h>
+
+using namespace OpenEngine::Core;
 void scheme_apply0(___SCMOBJ);
 
 ___SCMOBJ fun = NULL;
 
-void set_fun(___SCMOBJ new) {
-  fun = new;
+void set_fun(___SCMOBJ newz) {
+  fun = newz;
+
 }
 
 void call_fun() {
   scheme_apply0(fun);
 }
 
+template <class EventArg>
+class Wrap : public IListener<EventArg> {
+___SCMOBJ obj;
+public:
+Wrap(___SCMOBJ o) : obj(o) {
+logger.info << ___BODY(o) << logger.end;
+}
+
+void Handle(EventArg arg) {
+logger.info << ___BODY(obj) << logger.end;
+scheme_apply0(obj);
+}
+};
+
 header
 )
+
+(c-define-type IEngine "IEngine")
+(c-define-type IEngine* (pointer IEngine))
+
+
 
 (define (set-fun fun)
   ((c-lambda (scheme-object) void
@@ -50,3 +77,34 @@ header
       (set-fun (caar (make-garbage 100)))
       (##gc)
       (call-fun))))
+
+(define test-att 
+  (c-lambda (IEngine* scheme-object) void
+#<<ATT
+Wrap<ProcessEventArg> *w = new Wrap<ProcessEventArg>(___arg2);
+IEngine* eng = ___arg1;
+eng->ProcessEvent().Attach(*w);
+ATT
+))
+
+
+(c-define (set_engine e) (IEngine*) void "set_engine" ""
+          (print "wooo")
+          (newline)
+          (test-att e
+                    (##still-obj-refcount-inc!
+                      (##still-copy (lambda ()
+                                      (print "was?")
+                                      (newline)))))
+          )
+
+
+; (include "remote-debugger/debuggee.scm")
+; (make-rdi-host "localhost:20000")
+
+; (thread-start!
+;  (make-thread
+;   (lambda () (##repl-debug-main))))
+
+
+;(debug)
